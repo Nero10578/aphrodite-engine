e3b0ef2429511a9eb09923184590ed1fee4f61ca
Nero10578 <owenarliawan@gmail.com>
2025-11-22 03:19:00 -0800

    Revert "lora bias"
    This reverts commit f3120c3f57e7a9397915f11fdeaca6aac4e100d1.

--- aphrodite/lora/layers/base_linear.py
+++ aphrodite/lora/layers/base_linear.py
@@ -78,23 +78,12 @@ class BaseLinearLayerWithLoRA(BaseLayerWithLoRA):
             )
             for _ in range(self.n_slices)
         )
-        self.lora_bias_stacked = tuple(
-            torch.zeros(
-                max_loras,
-                1,
-                lora_b_out_size,
-                dtype=lora_config.lora_dtype,
-                device=self.device,
-            )
-            for _ in range(self.n_slices)
-        )
         self.output_slices = (self.lora_b_stacked[0].shape[2],)
 
     def reset_lora(self, index: int):
         for s_index in range(self.n_slices):
             self.lora_a_stacked[s_index][index] = 0
             self.lora_b_stacked[s_index][index] = 0
-            self.lora_bias_stacked[s_index][index] = 0
 
     def set_lora(
         self,
@@ -102,7 +91,6 @@ class BaseLinearLayerWithLoRA(BaseLayerWithLoRA):
         lora_a: torch.Tensor,
         lora_b: torch.Tensor,
         embeddings_tensor: torch.Tensor | None,
-        lora_bias: torch.Tensor | None = None,
     ):
         # Except for QKVParallelLinearWithLoRA and
         # MergedColumnParallelLinearWithLoRA, all other linear LoRA layers
@@ -114,15 +102,9 @@ class BaseLinearLayerWithLoRA(BaseLayerWithLoRA):
         if self.tp_size > 1:
             lora_a = self.slice_lora_a(lora_a)
             lora_b = self.slice_lora_b(lora_b)
-            if lora_bias is not None:
-                # Reuse slice_lora_b for bias since it has the same output dimension
-                # But slice_lora_b expects 2D tensor, bias is 1D
-                lora_bias = self.slice_lora_b(lora_bias.unsqueeze(1)).squeeze(1)
 
         self.lora_a_stacked[0][index, 0, : lora_a.shape[0], : lora_a.shape[1]].copy_(lora_a, non_blocking=True)
         self.lora_b_stacked[0][index, 0, : lora_b.shape[0], : lora_b.shape[1]].copy_(lora_b, non_blocking=True)
-        if lora_bias is not None:
-            self.lora_bias_stacked[0][index, 0, : lora_bias.shape[0]].copy_(lora_bias, non_blocking=True)
 
     def apply(
         self,
@@ -142,7 +124,10 @@ class BaseLinearLayerWithLoRA(BaseLayerWithLoRA):
         lora_output: torch.Tensor | None = self.punica_wrapper.add_lora_linear(
             output, x, self.lora_a_stacked, self.lora_b_stacked, 1.0, self.output_slices
         )
+<<<<<<< HEAD
 
+=======
+>>>>>>> parent of f3120c3f (lora bias)
         if not current_platform.can_update_inplace():
             output = lora_output
 
